a794e550d8fb1e1b0ce58197d050045a4fa88e94
Nnodimele Udodirim <42303171+Udodirim5@users.noreply.github.com>
2025-06-26 12:23:16 +0100

    refactor: uncomment route protection middleware for project routes

--- src/api/projects/project.routes.ts
+++ src/api/projects/project.routes.ts
@@ -1,7 +1,7 @@
 // routes/project.routes.ts
 import express, { Router } from 'express';
 import ProjectController from './project.controller';
-// import { protect, restrictToProjectAccess } from '../../middlewares/auth.middleware';
+import { protect, restrictToProjectAccess } from '../../middlewares/auth.middleware';
 import taskRoutes from '../tasks/task.routes';
 
 const router: Router = express.Router();
@@ -19,9 +19,8 @@ const {
 } = ProjectController;
 
 // ✅ Protect all routes
-// router.use(protect);
+router.use(protect);
 router.use('/:projectId/tasks', taskRoutes);
-// uncomment the above line to enable task routes under project
 
 // ✅ Routes
 router.route('/').post(createProject).get(getAllProjects);
@@ -29,34 +28,30 @@ router.route('/').post(createProject).get(getAllProjects);
 router
   .route('/:projectId')
   .get(
-    // restrictToProjectAccess(['OWNER', 'ADMIN', 'MEMBER']), // anyone in the project
+    restrictToProjectAccess(['OWNER', 'ADMIN', 'MEMBER']), // anyone in the project
     getProject,
   )
   .patch(
-    // restrictToProjectAccess(['OWNER', 'ADMIN']), // only owner/admin
+    restrictToProjectAccess(['OWNER', 'ADMIN']), // only owner/admin
     updateProject,
   )
   .delete(
-    // restrictToProjectAccess(['OWNER']), // only owner
+    restrictToProjectAccess(['OWNER']), // only owner
     deleteProject,
   );
 
-router.route('/:projectId/members/add').post(
-  // restrictToProjectAccess(['OWNER', 'ADMIN']),
-  addMember,
-);
+router
+  .route('/:projectId/members/add')
+  .post(restrictToProjectAccess(['OWNER', 'ADMIN']), addMember);
 
-router.route('/:projectId/members/remove').delete(
-  // restrictToProjectAccess(['OWNER', 'ADMIN']),
-  removeMember,
-);
-router.route('/:projectId/members/role/makeAdmin').patch(
-  // restrictToProjectAccess(['OWNER', 'ADMIN']),
-  makeAdmin,
-);
-router.route('/:projectId/members/role/remove-admin').patch(
-  // restrictToProjectAccess(['OWNER', 'ADMIN']),
-  removeAdmin,
-);
+router
+  .route('/:projectId/members/remove')
+  .delete(restrictToProjectAccess(['OWNER', 'ADMIN']), removeMember);
+router
+  .route('/:projectId/members/role/makeAdmin')
+  .patch(restrictToProjectAccess(['OWNER', 'ADMIN']), makeAdmin);
+router
+  .route('/:projectId/members/role/remove-admin')
+  .patch(restrictToProjectAccess(['OWNER', 'ADMIN']), removeAdmin);
 
 export default router;
